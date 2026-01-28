f232f688c2be668d8efaf1baeaf1a16f524a231d
krishnakantgunjal <krishnagunjal2003@gmail.com>
2026-01-28 22:14:00 +0530

    Auto-create superuser for Render free

---
+++ myproject/create_superuser.py
@@ -0,0 +1,23 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+USERNAME = "admin"
+EMAIL = "admin@example.com"
+PASSWORD = "admin@123"
+
+if not User.objects.filter(username=USERNAME).exists():
+    User.objects.create_superuser(
+        username=USERNAME,
+        email=EMAIL,
+        password=PASSWORD
+    )
+    print("Superuser created")
+else:
+    print("Superuser already exists")
